e52c8f984a27a1e8f063adb1f32a97b4facc6b7e
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-08-05 22:24:23 +0530

    Add PDF upload and import functionality for websites and YouTube; update source controller, services, and routes accordingly

--- server/src/routes/source.routes.ts
+++ server/src/routes/source.routes.ts
@@ -5,11 +5,22 @@ import {
     createSource,
     deleteSource,
     getSource,
+    importWebsite,
+    importYoutube,
     listSources,
+    uploadPdf,
 } from "../controllers/source.controller.js";
+import { uploadSinglePdf } from "../middleware/upload.middleware.js";
 
 export const sourceRoutes = Router({ mergeParams: true });
 
+sourceRoutes.post(
+    "/upload",
+    uploadSinglePdf,
+    asyncHandler(uploadPdf),
+);
+sourceRoutes.post("/import/website", asyncHandler(importWebsite));
+sourceRoutes.post("/import/youtube", asyncHandler(importYoutube));
 sourceRoutes.get("/", asyncHandler(listSources));
 sourceRoutes.post("/", asyncHandler(createSource));
 sourceRoutes.post("/bulk-delete", asyncHandler(bulkDeleteSources));
